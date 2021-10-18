43c2f752f375a2434c4f0d9a1c5818c25dc509e0
thanawat.bcr <thanwat.bcr@gmail.com>
2021-10-18 11:07:38 +0700

    [ADD] fonts & images

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.app Welcome to our Project
+.app
   .status {{ status }}
 </template>
 
@@ -20,19 +20,28 @@ export default App;
 </script>
 
 <style lang="scss">
-// .app {}
+*, *::before, *::after { @apply box-border m-0 p-0; }
+
+.app {
+  @apply flex justify-center items-center h-screen bg-center bg-cover;
+  background-image: url('./assets/images/app_background.png');
+}
+
+// SETUP FONTS
+@import url('https://fonts.googleapis.com/css2?family=Saira:wght@400;600&display=swap');
+* { font-family: 'Saira', sans-serif; }
 
 // SETUP PRIMARY AND SECONDARY COLOR HERE
 :root {
-  --color-primary-100: #E6FAF4;
-  --color-primary-200: #BFF4E3;
-  --color-primary-300: #99EDD3;
-  --color-primary-400: #4DDFB1;
-  --color-primary-500: #00D190;
-  --color-primary-600: #00BC82;
-  --color-primary-700: #007D56;
-  --color-primary-800: #005E41;
-  --color-primary-900: #003F2B;
+  --color-primary-100: #ffdeb7;
+  --color-primary-200: #ffc989;
+  --color-primary-300: #ffb35c;
+  --color-primary-400: #fea33f;
+  --color-primary-500: #fe932c;
+  --color-primary-600: #f9882a;
+  --color-primary-700: #f27828;
+  --color-primary-800: #eb6925;
+  --color-primary-900: #e14f21;
   --color-secondary-100: #E9EFF9;
   --color-secondary-200: #C7D7F1;
   --color-secondary-300: #A5BFE8;
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
+
 import App from './App.vue';
 import router from './router';
-import VueCompositionAPI from '@vue/composition-api'
 import './assets/tailwind.css';
 
-Vue.use(VueCompositionAPI)
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
